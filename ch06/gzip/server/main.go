7107985646d4d99fd7529ace87038bdaa2570c9a
budougumi0617 <budougumi0617@gmail.com>
2018-01-08 20:07:19 +0900

    :+1: Add server supported gzip

---
+++ ch06/gzip/server/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"compress/gzip"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"strings"
+	"time"
+)
+
+// isGZipAcceptable クライアントはgzipｗ受け入れ可能か？
+func isGZipAcceptable(request *http.Request) bool {
+	return strings.Index(
+		strings.Join(request.Header["Accept-Encoding"], ","),
+		"gzip") != -1
+}
+
+// processSession 1セッションの処理をする
+func processSession(conn net.conn) {
+	fmt.Fprintf("Accept %v\n", conn.RemoteAddr())
+	defer conn.Close()
+	for {
+		conn.SetReadDeadline(time.Now().Add(5 * time.Second))
+		// リクエストを読み込む
+		request, err := http.ReadRequest(bufio.NewReader(conn))
+		if err != nil {
+			neterr, ok := err.(net.Error)
+			if ok && neterr.Timeout() {
+				fmt.Println("Timeout")
+				break
+			} else if err == io.EOF {
+				break
+			}
+		}
+		panic(err)
+	}
+	dump, err := httputil.DumpRequest(request, true)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(dump))
+	// レスポンスを書き込む
+	response := http.Request{
+		StatusCode: 200,
+		ProtoMajor: 1,
+		ProtoMinor: 1,
+		Header:     make(http.Header),
+	}
+	if isGZipAcceptable(request) {
+		content := "Hello World (gzipped)\n"
+		// コンテンツをgzip化して転送
+		var buffer bytes.Buffer
+		writer := gzip.NewWriter(&buffer)
+		io.WriteString(writer, content)
+		writer.Close()
+		response.Body = ioutil.NopCloser(
+			strings.NewReader(content))
+		response.ContentLength = int64(len(content))
+	}
+	response.Write(conn)
+}
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8888")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Server is running at localhost:8888")
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		go processSession(conn)
+	}
+}
