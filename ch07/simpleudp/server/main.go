ed06865566000444e568f44eabcf3e0ef8f40e6e
budougumi0617 <budougumi0617@gmail.com>
2018-01-12 09:24:34 +0900

    :memo: Add comment

--- ch07/simpleudp/server/main.go
+++ ch07/simpleudp/server/main.go
@@ -8,14 +8,17 @@ import (
 
 func main() {
 	fmt.Println("Server is running ad localhost:8888")
+	// ListenPacketを使うと、クライアントを待たずにデータ送受信用のnet.PacketConnが即座に返される。
 	conn, err := net.ListenPacket("udp", "localhost:8888")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer conn.Close()
 
+	// データの終了が不明なまま受信するので、バッファをつくって読み込む
 	buffer := make([]byte, 1500)
 	for {
+		// ReadFromを使うと、接続してきた相手のアドレス情報を受け取れる
 		length, remoteAddress, err := conn.ReadFrom(buffer)
 		if err != nil {
 			log.Fatal(err)
